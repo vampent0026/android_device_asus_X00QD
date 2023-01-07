9e7bcb0ac1c6fa8ee30c8d9bba5ae8bfc8a776ad
MLZ94 <zugutoroberto@gmail.com>
2023-01-07 22:52:15 +0100

    X00QD: switch to lineage vibrator hal integration
    Signed-off-by: henricaodopao1 <dragonsxbr@gmail.com>

--- device.mk
+++ device.mk
@@ -479,7 +479,10 @@ $(call inherit-product, build/target/product/verity.mk)
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+#    android.hardware.vibrator@1.0-service
+
+PRODUCT_PACKAGES += \
+	android.hardware.vibrator@1.0-service.lineage
 
 # FIXME: master: compat for libprotobuf
 # See https://android-review.googlesource.com/c/platform/prebuilts/vndk/v28/+/1109518
