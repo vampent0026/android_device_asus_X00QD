cdb10e2ffe195f01ef2ba2a4bd95a9726b0cb2e4
SebaUbuntu <seba@sebaubuntu.dev>
2023-07-21 14:30:27 +0000

    sdm660-common: Don't explicitly build older minor version HIDL libs
    Change-Id: Ib0caa49054a79b1f01ffc751826286a29c642e06

--- sdm660.mk
+++ sdm660.mk
@@ -88,7 +88,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5 \
     android.hardware.camera.provider@2.6 \
     vendor.qti.hardware.camera.device@1.0
 
@@ -122,7 +121,6 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
     vendor.display.config@2.0 \
     vendor.qti.hardware.display.allocator-service \
-    vendor.qti.hardware.display.mapper@1.0.vendor \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor
 
