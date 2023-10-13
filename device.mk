272172900d9465244f1d45ef40b832f69f4b3d90
henricaodopao1 <dragonsxbr@gmail.com>
2023-10-13 10:07:14 +0000

    Revert "X00QD: switch to lineage vibrator hal integration"
    This reverts commit 9e7bcb0ac1c6fa8ee30c8d9bba5ae8bfc8a776ad.

--- device.mk
+++ device.mk
@@ -470,10 +470,7 @@ PRODUCT_PACKAGES += \
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
-#    android.hardware.vibrator@1.0-service
-
-PRODUCT_PACKAGES += \
-	android.hardware.vibrator@1.0-service.lineage
+    android.hardware.vibrator@1.0-service
 
 # FIXME: master: compat for libprotobuf
 # See https://android-review.googlesource.com/c/platform/prebuilts/vndk/v28/+/1109518
