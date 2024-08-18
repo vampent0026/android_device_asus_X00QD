563a90fac1a944f84d9371af02341aecb75d3714
vampent0026 <bencena0026@gmail.com>
2024-08-18 22:47:28 +0800

    Initial derp14

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,10 +5,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00QD.mk
+    $(LOCAL_DIR)/derp_X00QD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00QD-user \
-    lineage_X00QD-userdebug \
-    lineage_X00QD-eng
+    derp_X00QD-user \
+    derp_X00QD-userdebug \
+    derp_X00QD-eng
 
